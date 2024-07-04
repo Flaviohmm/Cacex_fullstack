3930238b4c086f49f8854e6a52bd639af5f11b57
Flavio Macedo <fhenrique609@gmail.com>
2024-07-04 10:47:56 -0300

    Estilização dos Botoes

--- frontend/cacex-frontend-master/src/components/Welcome.tsx
+++ frontend/cacex-frontend-master/src/components/Welcome.tsx
@@ -15,18 +15,18 @@ const Welcome: React.FC = () => {
             <Header />
             <main className="flex-grow flex justify-center items-center p-4">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                    <button className="bg-blue-700 text-white p-6 rounded hover:bg-blue-500">CAIXA</button>
-                    <button className="bg-blue-700 text-white p-6 rounded hover:bg-blue-500">ESTADO</button>
-                    <button className="bg-blue-700 text-white p-6 rounded hover:bg-blue-500">FNDE</button>
-                    <button className="bg-blue-700 text-white p-6 rounded hover:bg-blue-500">SIMEC</button>
-                    <button className="bg-blue-700 text-white p-6 rounded hover:bg-blue-500">FNS</button>
-                    <button className="bg-blue-700 text-white p-6 rounded hover:bg-blue-500">ENTIDADE</button>
-                    <button className="bg-blue-700 text-white p-6 rounded hover:bg-blue-500">PREVIDÊNCIA</button>
-                    <button className="bg-blue-700 text-white p-6 rounded hover:bg-blue-500">FGTS</button>
-                    <button className="bg-blue-700 text-white p-6 rounded hover:bg-blue-500">RECEITA FEDERAL</button>
-                    <button className="bg-blue-700 text-white p-6 rounded hover:bg-blue-500">ADMINISTRAÇÃO</button>
-                    <button className="bg-blue-700 text-white p-6 rounded hover:bg-blue-500">CONTABILIDADE</button>
-                    <button className="bg-blue-700 text-white p-6 rounded hover:bg-blue-500">INDIVIDUALIZAÇÃO</button>
+                    <button className="bg-blue-700 text-white p-10 rounded-xl hover:bg-blue-500">CAIXA</button>
+                    <button className="bg-blue-700 text-white p-10 rounded-xl hover:bg-blue-500">ESTADO</button>
+                    <button className="bg-blue-700 text-white p-10 rounded-xl hover:bg-blue-500">FNDE</button>
+                    <button className="bg-blue-700 text-white p-10 rounded-xl hover:bg-blue-500">SIMEC</button>
+                    <button className="bg-blue-700 text-white p-10 rounded-xl hover:bg-blue-500">FNS</button>
+                    <button className="bg-blue-700 text-white p-10 rounded-xl hover:bg-blue-500">ENTIDADE</button>
+                    <button className="bg-blue-700 text-white p-10 rounded-xl hover:bg-blue-500">PREVIDÊNCIA</button>
+                    <button className="bg-blue-700 text-white p-10 rounded-xl hover:bg-blue-500">FGTS</button>
+                    <button className="bg-blue-700 text-white p-10 rounded-xl hover:bg-blue-500">RECEITA FEDERAL</button>
+                    <button className="bg-blue-700 text-white p-10 rounded-xl hover:bg-blue-500">ADMINISTRAÇÃO</button>
+                    <button className="bg-blue-700 text-white p-10 rounded-xl hover:bg-blue-500">CONTABILIDADE</button>
+                    <button className="bg-blue-700 text-white p-10 rounded-xl hover:bg-blue-500">INDIVIDUALIZAÇÃO</button>
                 </div>
             </main>         
         </div>
